bfa67c0d643d2b890cf2f7033a1c04aaa4aa6620
Siddharth Kundu <sk31395@gmail.com>
2018-08-19 17:20:46 +0530

    Initial Commit

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,47 @@
 const initialState = {
-  counter: 0
+  counter: 0,
+  results: []
 };
 
 const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        ...state,
+        counter: state.counter + 1
+      };
+    case "DECREMENT":
+      return {
+        ...state,
+        counter: state.counter - 1
+      };
+    case "ADD":
+      return {
+        ...state,
+        counter: state.counter + action.value
+      };
+    case "SUBTRACT":
+      return {
+        ...state,
+        counter: state.counter - action.value
+      };
+    case "STORE_RESULT":
+      return {
+        ...state,
+        results: state.results.concat({ id: new Date(), value: state.counter })
+      };
+    case "DELETE_RESULT":
+      // const id = 2;
+      // const newArray = [...state.results];
+      // state.results.id(id, 1);
+      const updatedArray = state.results.filter(
+        result => result.id !== action.resultElId
+      );
+      return {
+        ...state,
+        results: updatedArray
+      };
+  }
   return state;
 };
 
